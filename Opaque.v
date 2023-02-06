108b789d581e54829d9cfae8464b32edd0cdd6cc
Mukesh Tiwari <mukeshtiwari.iiitm@gmail.com>
2023-02-06 21:45:20 +1100

    Update Opaque.v

--- Opaque.v
+++ Opaque.v
@@ -4,6 +4,12 @@
    https://sympa.inria.fr/sympa/arc/coq-club/2013-09/msg00034.html?ticket=ST-272822-JjlPeYflOHbYh07SIzDGXuXTxLk-cas.inria.fr
 *)
 
+From Coq Require Import Arith Utf8 Psatz
+  Peano NArith Strings.Byte NArith.
+
+  Local Open Scope N_scope.
+
+
   Fixpoint guard A (R : A -> A -> Prop) (n : nat) (wfR : well_founded R) : well_founded R :=
     match n with
     | 0%nat => wfR
@@ -11,6 +17,38 @@
     end.
 
 
+  Definition np_total (np : N):  (np <? 256 = true) ->  byte.
+  Proof.
+    intros H.
+    pose proof of_N_None_iff np as Hn.
+    destruct (of_N np) as [b | ].
+    + exact b.
+    + exfalso; abstract (apply N.ltb_lt in H;
+        intuition nia). 
+  Defined.
+
+  Lemma nmod_256 : forall np, np mod 256 < 256.
+  Proof. 
+    intros; eapply N.mod_lt; intro H;
+    inversion H.
+  Qed. 
+
+
+  Lemma N_eq_gt : forall up t : N, 256 <= up -> t = up / 256 -> 0 <=  t / 256 < t.
+  Proof.
+    intros up t Hup Ht.
+    split. 
+    apply N.le_0_l.
+    apply N.div_lt_upper_bound. 
+    nia.
+    (* I know that 1 <= t *)
+    assert (Hot: 1 <= t).
+    rewrite Ht.
+    apply N.div_le_lower_bound.
+    nia.
+    simpl. exact Hup. nia.
+  Qed.
+  
   Definition length_byte_list (np : N) : list byte.
   Proof.
     refine ((fix length_byte_list up (H: Acc (fun x y => 0 <= x < y) (N.div up 256)) {struct H}:= 
@@ -22,8 +60,8 @@
         List.cons (np_total r _) (length_byte_list t _)
       end eq_refl
     ) np (guard _ (fun x y => 0 <= x < y) 100 (N.lt_wf 0) (N.div np 256))).
-    + apply N.ltb_lt in Hnp; exact Hnp.
-    + apply (nmod_256 up).
+    + exact Hnp.
+    + apply N.ltb_lt. apply (nmod_256 up).
     + eapply Acc_inv with (x := t). exact H.
       apply N.ltb_ge in Hnp.
       eapply N_eq_gt with (up := up); auto. 
