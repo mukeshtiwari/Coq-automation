e1afa236f39c20f8471aca5d5206979fb6dc519b
Mukesh Tiwari <mukeshtiwari.iiitm@gmail.com>
2023-02-06 22:31:52 +1100

    Update Opaque.v

--- Opaque.v
+++ Opaque.v
@@ -68,3 +68,40 @@ From Coq Require Import Arith Utf8 Psatz
   Defined.
 
   Eval compute in length_byte_list 256.
+  
+  
+  (* So it seems we can compute without guard condition as well *)
+  
+  Definition length_byte_list_without_guard : N -> list byte.
+  Proof.
+    intro np.
+    cut (Acc lt (N.to_nat np));
+    [revert np |].
+    +
+      refine(fix length_byte_list_without_guard np (acc : Acc lt (N.to_nat np))
+        {struct acc} := 
+      match acc with 
+      | Acc_intro _ f => 
+        match (np <? 256) as npp 
+          return _ = npp -> _ 
+        with
+        | true => fun Ha => List.cons (np_total np Ha) List.nil
+        | false => fun Ha => 
+            let r := N.modulo np 256 in 
+            let t := N.div np 256 in 
+            List.cons (np_total r _) (length_byte_list_without_guard t _)
+        end eq_refl
+      end).
+      ++
+        apply N.ltb_lt, (nmod_256 np).
+      ++
+        apply N.ltb_ge in Ha.
+        assert (Hc : (N.to_nat t < N.to_nat np)%nat).
+        unfold t. rewrite N2Nat.inj_div.
+        eapply Nat.div_lt; try abstract nia.
+        exact (f (N.to_nat t) Hc).
+    +
+      apply lt_wf.
+  Defined.
+
+  Eval compute in length_byte_list_without_guard 3723.
